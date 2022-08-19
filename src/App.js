ba4f21fee7307da23612966b47341305cc638b47
Seda Kara <sedaakaraa02@gmail.com>
2022-08-19 13:58:13 +0300

    ReactContextFilmProjesi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./lib/fontawesome/css/all.min.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Watchlist from "./components/Watchlist";
+import Watched from "./components/Watched";
+import Add from "./components/Add";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Watchlist />}></Route>
+        <Route path="/watched" element={<Watched />}></Route>
+        <Route path="/add" element={<Add/>}></Route>
+      </Routes>
+    </Router>
+    </GlobalProvider>
+
   );
 }
 
